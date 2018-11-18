0e0c2ae000cfde18c4d5247326184a86a6d278b0
zengyong <1013828761@qq.com>
2018-11-18 16:55:40 +0800

    首页

---
+++ Banana/Main/Home/HomeCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeCell.h
+//  Banana
+//
+//  Created by 曾勇 on 2018/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *borrowNOlabel;
+
+@end
--- Banana/Util/BConstant.h
+++ Banana/Util/BConstant.h
@@ -8,7 +8,14 @@
 #ifndef BConstant_h
 #define BConstant_h
 
+//    系统控件默认高度
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height//获取设备屏幕的长
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width//获取设备屏幕的宽
+#define ZoomScall kScreenWidth/375.0 //UI适配宽度比
+#define kStateHeight [[UIApplication sharedApplication] statusBarFrame].size.height //状态栏高度
+#define kNavHeight ([[UIApplication sharedApplication] statusBarFrame].size.height + 44) //导航栏+状态栏高度
+#define kTabbarHeight  (([[UIApplication sharedApplication] statusBarFrame].size.height) > (20) ? 83 : 49 ) //底部tabbar高度
+
+#define WS(weakSelf) __weak typeof(self) weakSelf = self;
 
 #endif /* BConstant_h */
