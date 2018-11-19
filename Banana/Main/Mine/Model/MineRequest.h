e2faa4ba388f3dae3111ddb956d4a7f7f2861d14
zb1006801184 <www.1006801184@qq.com>
2018-11-19 19:08:52 +0800

    Request

---
+++ Banana/Main/Mine/Model/MineRequest.h
@@ -0,0 +1,16 @@
+//
+//  MineRequest.h
+//  Banana
+//
+//  Created by 朱标 on 2018/11/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MineRequest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Banana/Main/Mine/Model/MineRequest.m
@@ -0,0 +1,12 @@
+//
+//  MineRequest.m
+//  Banana
+//
+//  Created by 朱标 on 2018/11/19.
+//
+
+#import "MineRequest.h"
+
+@implementation MineRequest
+
+@end
